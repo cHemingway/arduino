3af6bb2c4554dbe84b76572936bc9c63108b05a7
Mark Hazell <Evo-Ark@users.noreply.github.com>
2018-11-11 13:44:09 +0000

    Added a new part to the JSON schema for partID. Separated out responses to various mTypes into their own methods and created a setup program to configure Arduino memory

---
+++ arduino-main/setup-arduino-A.ino
@@ -0,0 +1,29 @@
+/*
+ * EEPROM Clear
+ *
+ * Sets all of the bytes of the EEPROM to 0.
+ * Please see eeprom_iteration for a more in depth
+ * look at how to traverse the EEPROM.
+ *
+ * This example code is in the public domain.
+ */
+
+#include <EEPROM.h>
+
+void setup() {
+  // initialize the LED pin as an output.
+  pinMode(13, OUTPUT);
+
+  EEPROM.write(0, 'A');
+  EEPROM.write(1, '1');
+ 
+ // turn the LED on when we're done
+  digitalWrite(13, HIGH);
+
+
+
+}
+
+void loop() {
+  /** Empty loop. **/
+}
